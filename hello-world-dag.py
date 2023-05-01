56ac918aa4197dfb03c7c1e762c54e5db140f8c6
Moises E. Benzan M <moises.benzan@canonical.com>
2023-05-01 11:07:01 -0400

    initial

---
+++ hello-world-dag.py
@@ -0,0 +1,10 @@
+import pendulum
+
+with DAG(
+    dag_id="my_dag",
+    start_date=pendulum.datetime(2016, 1, 1),
+    schedule="@daily",
+    default_args={"retries": 2},
+):
+    op = BashOperator(task_id="hello_world", bash_command="Hello World!")
+    print(op.retries)  # 2
